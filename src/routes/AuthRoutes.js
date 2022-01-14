32d202e84779b85a0a363f16e4f845ead7d24cac
Diego Salas <diesalasmart@gmail.com>
2022-01-14 11:04:19 -0500

    delete unused files

--- src/routes/AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const { login } = require('../controllers/AuthController')
-
-const router = express.Router();
-
-router.post('/login', login);
-
-module.exports = router
